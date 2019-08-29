f6ae3c90dfdb4a93591500f0e868010c98d1249e
wrkzdev <team@wrkz.work>
2019-08-29 04:18:07 +0200

    Remove duplicated directory

--- src/external/cryptopp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/derogold-dev/rev-rebase-test/external/cryptopp
-# Build directory: /root/derogold-dev/rev-rebase-test/src/external/cryptopp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
