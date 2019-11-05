d3514c5e77eba4913c454f92ccd368510102ecc7
CuvéeTRTL Node <leos.stehlik@gmail.com>
2019-11-05 15:10:14 +0000

    updated DB r/w buffers, DB threads and open files

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -282,10 +282,10 @@ namespace CryptoNote
     const size_t P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT = 5000; // 5 seconds
     const char P2P_STAT_TRUSTED_PUB_KEY[] = "";
 
-    const uint64_t DATABASE_WRITE_BUFFER_MB_DEFAULT_SIZE = 1024; // 1024 MB
-    const uint64_t DATABASE_READ_BUFFER_MB_DEFAULT_SIZE = 1024; // 1024 MB
-    const uint32_t DATABASE_DEFAULT_MAX_OPEN_FILES = 128; // 128 max. files. Highend server just use -1 for no limit
-    const uint16_t DATABASE_DEFAULT_BACKGROUND_THREADS_COUNT = 8; // 8 DB IncreaseParallelism
+    const uint64_t DATABASE_WRITE_BUFFER_MB_DEFAULT_SIZE = 256; // 256 MB
+    const uint64_t DATABASE_READ_BUFFER_MB_DEFAULT_SIZE = 512; // 512 MB
+    const uint32_t DATABASE_DEFAULT_MAX_OPEN_FILES = 64; // 64 max. files. Highend server just use -1 for no limit
+    const uint16_t DATABASE_DEFAULT_BACKGROUND_THREADS_COUNT = 4; // 4 DB IncreaseParallelism
     const uint64_t DATABASE_MAX_BYTES_FOR_LEVEL_BASE = 20 * DATABASE_WRITE_BUFFER_MB_DEFAULT_SIZE; // Additional tweak testing
 
     const char LATEST_VERSION_URL[] = "https://github.com/derogold/derogold/releases";
