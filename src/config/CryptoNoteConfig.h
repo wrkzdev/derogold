e214f652ac477c2b322b0c46c8f4280dc82ee282
CuvéeTRTL Node <leos.stehlik@gmail.com>
2019-11-05 15:13:39 +0000

    bumped up p2p version, updated hard fork height

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -128,7 +128,7 @@ namespace CryptoNote
 
         const uint64_t MAX_EXTRA_SIZE_V3 = 512;
 
-		const uint64_t MAX_EXTRA_SIZE_V3_HEIGHT = 1470000;
+	const uint64_t MAX_EXTRA_SIZE_V3_HEIGHT = 1470000;
 
         /* For new projects forked from this code base, the values immediately below
            should be changed to 0 to prevent issues with transaction processing
@@ -179,7 +179,7 @@ namespace CryptoNote
         const uint64_t FORK_HEIGHTS[] = {
             700000, // 0
             1470000, // 1
-            2000000, // 2
+            2100000, // 2
         };
 
         /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
@@ -256,9 +256,9 @@ namespace CryptoNote
 
     // P2P Network Configuration Section - This defines our current P2P network version
     // and the minimum version for communication between nodes
-    const uint8_t P2P_CURRENT_VERSION = 2;
+    const uint8_t P2P_CURRENT_VERSION = 3;
 
-    const uint8_t P2P_MINIMUM_VERSION = 1;
+    const uint8_t P2P_MINIMUM_VERSION = 2;
 
     // This defines the minimum P2P version required for lite blocks propogation
     const uint8_t P2P_LITE_BLOCKS_PROPOGATION_VERSION = 0;
