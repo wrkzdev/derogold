6eef930fb9446bdcd5cc5a04232b05405c17113c
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2019-12-07 05:27:14 +0700

    Adjust NORMAL_TX_OUTPUT_COUNT_LIMIT_V1

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -165,7 +165,7 @@ namespace CryptoNote
             to help curtail fusion transaction spam. */
         const size_t FUSION_TX_MAX_POOL_COUNT = 60;
 
-        const size_t NORMAL_TX_MAX_OUTPUT_RATIO_V1 = 30;
+        const size_t NORMAL_TX_MAX_OUTPUT_RATIO_V1 = 90;
 
         /* Similar to above. This is for the dust fusion to limit in the pool. */
         const size_t FUSION_TX_MAX_POOL_COUNT_FOR_AMOUNT_V1 = 20;
@@ -186,7 +186,7 @@ namespace CryptoNote
 
         const size_t NORMAL_TX_OUTPUT_SUM_MIN_V1 = UINT64_C(1000000);
 
-        const size_t NORMAL_TX_OUTPUT_COUNT_LIMIT_V1 = 300;
+        const size_t NORMAL_TX_OUTPUT_COUNT_LIMIT_V1 = 450;
 
         const size_t NORMAL_TX_OUTPUT_EACH_AMOUNT_V1 = UINT64_C(10000);
 
