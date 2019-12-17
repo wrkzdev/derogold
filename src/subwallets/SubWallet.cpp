18abb144a00dae44f6041f68af571f73ca90d341
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2019-12-18 05:39:08 +0700

    Revert Add isInputDustActivated

--- src/subwallets/SubWallet.cpp
+++ src/subwallets/SubWallet.cpp
@@ -365,10 +365,7 @@ std::vector<WalletTypes::TxInputAndOwner> SubWallet::getSpendableInputs(const ui
     {
         if (Utilities::isInputUnlocked(input.unlockTime, height))
         {
-            if (input.amount >= CryptoNote::parameters::DEFAULT_DUST_THRESHOLD_V2)
-            {
-                inputs.emplace_back(input, m_publicSpendKey, m_privateSpendKey);
-            }
+            inputs.emplace_back(input, m_publicSpendKey, m_privateSpendKey);
         }
     }
     return inputs;
