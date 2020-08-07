b971816166b9c1414868a2cbe0bf2e7cfc73fb1f
CuvéeTRTL Node <leos.stehlik@gmail.com>
2020-08-07 22:10:39 +0000

    Increase fusion txs limit in the mempool

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -188,7 +188,7 @@ namespace CryptoNote
             at any given time. Incoming fusion transactions that attempt to exceed this limit
             will be rejected from the pool and will not be added. This mechanism is in place
             to help curtail fusion transaction spam. */
-        const size_t FUSION_TX_MAX_POOL_COUNT = 60;
+        const size_t FUSION_TX_MAX_POOL_COUNT = 1660;
 
         /* We just set it output max to 90 */
         const size_t NORMAL_TX_MAX_OUTPUT_COUNT_V1  = 90;
